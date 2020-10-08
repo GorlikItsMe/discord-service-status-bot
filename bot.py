d21273564acbca0ca7c4ea11d89abb908cf21070
Gorlik <33355238+GorlikItsMe@users.noreply.github.com>
2020-10-08 18:32:58 +0200

    heroku prototype

--- bot.py
+++ bot.py
@@ -1,19 +1,24 @@
 import discord
 import json
 import sys
+import os
 
 """
 SETUP
 """
-try:
-    fp = open('config.json')
-    CONFIG = json.load(fp)
-    fp.close()
-except:
-    print("ERROR")
-    print("config.json dont exist, change name sample.config.json to config.json")
-    print("Dont forget to change config")
-    sys.exit(0)
+CONFIG = os.environ.get('CONFIG',False)
+
+if CONFIG == False:
+    try:
+        fp = open('config.json')
+        CONFIG = json.load(fp)
+        fp.close()
+    except:
+        print("ERROR")
+        print("config.json dont exist, change name sample.config.json to config.json")
+        print("If you use heroku you dont set CONFIG value")
+        print("use sample.config.json to configure")
+        sys.exit(0)
 
 
 
